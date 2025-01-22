b3e0c0db94a7e5f946c322702209737de84df19d
akshay310 <70632287+akshay310@users.noreply.github.com>
2025-01-22 17:22:25 +0530

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -1,3 +1,12 @@
+--Query to get the current inventory for each food bank
+SELECT fb.bank_name, fi.item_name, SUM(di.quantity) AS current_inventory
+FROM NGO.Food_Banks fb
+JOIN NGO.Donations d ON fb.bank_id = d.bank_id
+JOIN NGO.Donation_Items di ON d.donation_id = di.donation_id
+JOIN NGO.Food_Items fi ON di.item_id = fi.item_id
+GROUP BY fb.bank_name, fi.item_name
+ORDER BY fb.bank_name, fi.item_name;
+
 --Find the total quantity of each food item donated to each food bank in March 2024
 SELECT fb.bank_name, fi.item_name, SUM(di.quantity) AS total_quantity
 FROM NGO.Food_Banks fb
