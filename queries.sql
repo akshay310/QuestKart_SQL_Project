919d63f5e17220bc51f4821009ebc6c931f47a68
akshay310 <70632287+akshay310@users.noreply.github.com>
2025-01-22 14:49:50 +0530

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -1,10 +1,10 @@
--- Find the total quantity of each food item donated to each food bank in March 2024
+--Find the total quantity of each food item donated to each food bank in March 2024
 SELECT fb.bank_name, fi.item_name, SUM(di.quantity) AS total_quantity
 FROM NGO.Food_Banks fb
 JOIN NGO.Donations d ON fb.bank_id = d.bank_id
 JOIN NGO.Donation_Items di ON d.donation_id = di.donation_id
 JOIN NGO.Food_Items fi ON di.item_id = fi.item_id
-WHERE EXTRACT(MONTH FROM d.donation_date) = 3 AND EXTRACT(YEAR FROM d.donation_date) = 2024 
+WHERE EXTRACT(MONTH FROM d.donation_date) = 3 AND EXTRACT(YEAR FROM d.donation_date) = 2024 -- Filter for March 2024
 GROUP BY fb.bank_name, fi.item_name
 ORDER BY fb.bank_name, total_quantity DESC;
 
@@ -17,4 +17,24 @@ GROUP BY don.donor_name
 ORDER BY total_donated_quantity DESC
 LIMIT 3;
 
+--Find the food bank with the highest quantity of donations
+SELECT fb.bank_name, SUM(di.quantity) AS total_donated_quantity
+FROM NGO.Food_Banks fb
+JOIN NGO.Donations d ON fb.bank_id = d.bank_id
+JOIN NGO.Donation_Items di ON d.donation_id = di.donation_id
+GROUP BY fb.bank_name
+ORDER BY total_donated_quantity DESC
 
+--Find the average quantity of each food item donated across all donations:
+SELECT fi.item_name, AVG(di.quantity) AS average_donated_quantity
+FROM NGO.Food_Items fi
+JOIN NGO.Donation_Items di ON fi.item_id = di.item_id
+GROUP BY fi.item_name
+ORDER BY average_donated_quantity DESC;
+
+--Find the total number of volunteer hours worked at each food bank 
+SELECT fb.bank_name, COALESCE(SUM(EXTRACT(EPOCH FROM (vs.end_time - vs.start_time)) / 3600), 0) AS total_volunteer_hours
+FROM NGO.Food_Banks fb
+LEFT JOIN NGO.Volunteer_Shifts vs ON fb.bank_id = vs.bank_id
+GROUP BY fb.bank_name
+ORDER BY total_volunteer_hours DESC;
